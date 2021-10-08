881d8a598744734533a1f0558173e20d17b5f1d2
Amirul Islam <amirulislamalmamun@gmail.com>
2021-10-09 01:06:23 +0600

    use effect hook

--- src/hooks-practice/Timer.js
+++ src/hooks-practice/Timer.js
@@ -5,10 +5,13 @@ function Timer() {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        setTimeout(() => {
+        let timer = setTimeout(() => {
             setCount(count => ++count);
         }, 1000);
-    }, []);
+
+        return () => clearTimeout(timer);
+    }, []); // run only once
+    // }); // run infinitely
 
     return <h2>Rendered <h1 style={{color:"red"}}>{count}</h1> times!</h2>
 }
